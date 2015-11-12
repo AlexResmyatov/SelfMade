b7b78a1c2ae15e7a684f0d67a5beba2306737b08
AlexResmyatov <alex.resmyatov@gmail.com>
2015-11-12 20:52:47 +0600

    20 52

---
+++ core/src/com/mrgradus/screens/IScreen.java
@@ -0,0 +1,8 @@
+package com.mrgradus.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public interface IScreen extends Screen{
+	public void setScreen(Game game);
+}
---
+++ core/src/com/mrgradus/screens/TestScreen.java
@@ -0,0 +1,62 @@
+package com.mrgradus.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+public class TestScreen implements IScreen{
+
+	Game game;
+	
+	public TestScreen(Game game) {
+		this.game=game;
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.app.log("TestScreen","render");
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setScreen(Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
