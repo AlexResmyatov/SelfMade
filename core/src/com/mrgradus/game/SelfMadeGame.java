677565e29877cd9863cd850f113b7709eaa7ea8e
AlexResmyatov <alex.resmyatov@gmail.com>
2015-11-12 20:47:14 +0600

    IScreen

--- core/src/com/mrgradus/game/SelfMadeGame.java
+++ core/src/com/mrgradus/game/SelfMadeGame.java
@@ -1,28 +1,31 @@
 package com.mrgradus.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mrgradus.screens.TestScreen;
 
-public class SelfMadeGame extends ApplicationAdapter {
+public class SelfMadeGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+		this.setScreen(new TestScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-		dbgfgb44444444444444444
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+		super.render();
 	}
 }
